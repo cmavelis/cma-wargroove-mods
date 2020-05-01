a6fc8537cf5b788b4d953aa2000b05342f8775dc
CMAvelis <CMAvelis@gmail.com>
2020-05-01 16:30:18 -0400

    vampiric touch teleport and animation

--- gow_co_balance_mod/assets_src/lua/initialized/groove_vampiric_touch.lua
+++ gow_co_balance_mod/assets_src/lua/initialized/groove_vampiric_touch.lua
@@ -7,45 +7,95 @@ local VampiricTouch = GrooveVerb:new()
 VampiricTouch.isInPreExecute = false
 
 function VampiricTouch:init()
+    OldVampiricTouch.canExecuteWithTarget = VampiricTouch.canExecuteWithTarget
     OldVampiricTouch.execute = VampiricTouch.execute
+    OldVampiricTouch.getMaximumRange = VampiricTouch.getMaximumRange
+    OldVampiricTouch.getTargetType = VampiricTouch.getTargetType
+    OldVampiricTouch.onPostUpdateUnit = VampiricTouch.onPostUpdateUnit
 end
 
 function VampiricTouch:getMaximumRange(unit, endPos)
-    return 1
-end
+    if VampiricTouch.isInPreExecute then
+        return 1
+    end
 
+    return 2
+end
 
 function VampiricTouch:getTargetType()
-    return "unit"
+    if VampiricTouch.isInPreExecute then
+        return "all" -- enemy?
+    end
+
+    return "empty"
 end
+
+-- function VampiricTouch:getTargets(unit)
+
+-- function VampiricTouch:preExecute(unit, targetPos, strParam, endPos)
+--     VampiricTouch.isInPreExecute = true
+--     Wargroove.selectTarget()
+
+--     while Wargroove.waitingForSelectedTarget() do
+--         coroutine.yield()
+--     end
+
+--     local targetUnit = Wargroove.getSelectedTarget()
+
+--     if (targetUnit == nil) then
+--         VampiricTouch.isInPreExecute = false
+--         return false, ""
+--     end
+
+--     Wargroove.setSelectedTarget(targetPos)
+--     VampiricTouch.isInPreExecute = false
+
+--     return true, "string"
+-- end
+
+
+-- function VampiricTouch:canExecuteAt(unit, endPos)
+--     if not Verb.canExecuteAt(self, unit, endPos) then
+--         return false
+--     end
+    
+--     local targets = self:getTargets(unit, endPos, {}, false)
+--     return #targets > 0
+-- end
 
 function VampiricTouch:canExecuteWithTarget(unit, endPos, targetPos, strParam)
     if not self:canSeeTarget(targetPos) then
         return false
     end
 
-    local targetUnit = Wargroove.getUnitAt(targetPos)
-
-    if not targetUnit or not Wargroove.areEnemies(unit.playerId, targetUnit.playerId) or (not targetUnit.canBeAttacked) then
-        return false
-    end
-
-    if targetUnit.unitClass.isCommander or targetUnit.unitClass.isStructure then
-        return false
-    end
-
-    for i, tag in ipairs(targetUnit.unitClass.tags) do
-        if tag == "summon" then
-            return false
-        end
-    end
-
-    return true
+    local u = Wargroove.getUnitAt(targetPos)
+    local uc = Wargroove.getUnitClass("soldier")
+    return (u == nil or u.id == unit.id) and Wargroove.canStandAt("soldier", targetPos)
 end
+    -- -- this all goes into a different function FROM here
+    -- local targetUnit = Wargroove.getUnitAt(targetPos)
+
+    -- if not targetUnit or not Wargroove.areEnemies(unit.playerId, targetUnit.playerId) or (not targetUnit.canBeAttacked) then
+    --     return false
+    -- end
+
+    -- if targetUnit.unitClass.isCommander or targetUnit.unitClass.isStructure then
+    --     return false
+    -- end
+
+    -- for i, tag in ipairs(targetUnit.unitClass.tags) do
+    --     if tag == "summon" then
+    --         return false
+    --     end
+    -- end
+    -- -- TO here
+-- end
 
 function VampiricTouch:execute(unit, targetPos, strParam, path)
     Wargroove.setIsUsingGroove(unit.id, true)
     Wargroove.updateUnit(unit)
+    print('ongroove',unit.pos.x)
+
 
     Wargroove.playPositionlessSound("battleStart")
     Wargroove.playGrooveCutscene(unit.id)
@@ -65,22 +115,32 @@ function VampiricTouch:execute(unit, targetPos, strParam, path)
     end
 
     Wargroove.setFacingOverride(unit.id, facingOverride)
-
     Wargroove.playUnitAnimation(unit.id, "groove")
+
     Wargroove.playMapSound("sigrid/sigridGroove", targetPos)
-    Wargroove.waitTime(0)
 
-    Wargroove.spawnMapAnimation(unit.pos, 0, "fx/groove/sigrid_groove_fx", effectSequence, "over_units", { x = 12, y = 12 }, facingOverride)
-
+    Wargroove.spawnMapAnimation(targetPos, 0, "fx/groove/sigrid_groove_fx", effectSequence, "over_units", { x = 12, y = 12 }, facingOverride)
     Wargroove.waitTime(1.0)
-    
-    local u = Wargroove.getUnitAt(targetPos)
-    if u.health then
-        unit:setHealth(unit.health + u.health, unit.id)
-        u:setHealth(0, unit.id)
-        Wargroove.updateUnit(u)
-        Wargroove.playUnitAnimation(u.id, "hit")
+    Wargroove.moveUnitToOverride(unit.id, targetPos, 0, 0, 5)
+    while (Wargroove.isLuaMoving(unit.id)) do
+        coroutine.yield()
     end
+    
+    print('after move',unit.pos.x)
+
+    unit.pos = { x = targetPos.x, y = targetPos.y }
+    print('beforeupdate',unit.pos.x)
+
+    Wargroove.updateUnit(unit)
+    print('afterupdate',unit.pos.x)
+    
+    -- local u = Wargroove.getUnitAt(targetPos)
+    -- if u.health then
+    --     unit:setHealth(unit.health + u.health, unit.id)
+    --     u:setHealth(0, unit.id)
+    --     Wargroove.updateUnit(u)
+    --     Wargroove.playUnitAnimation(u.id, "hit")
+    -- end
 
     Wargroove.playGrooveEffect()
 
@@ -89,57 +149,9 @@ function VampiricTouch:execute(unit, targetPos, strParam, path)
     Wargroove.waitTime(1.0)
 end
 
-function VampiricTouch:generateOrders(unitId, canMove)
-    local orders = {}
-
-    local unit = Wargroove.getUnitById(unitId)
-    local unitClass = Wargroove.getUnitClass(unit.unitClassId)
-    local movePositions = {}
-    if canMove then
-        movePositions = Wargroove.getTargetsInRange(unit.pos, unitClass.moveRange, "empty")
-    end
-    table.insert(movePositions, unit.pos)
-
-    for i, pos in pairs(movePositions) do
-        local targets = Wargroove.getTargetsInRangeAfterMove(unit, pos, pos, 1, "unit")
-        for j, targetPos in pairs(targets) do
-            local u = Wargroove.getUnitAt(targetPos)
-            if u ~= nil then
-                local uc = Wargroove.getUnitClass(u.unitClassId)
-                if self:canExecuteWithTarget(unit, pos, targetPos, "") and not Wargroove.hasAIRestriction(u.id, "dont_target_this") then
-                    orders[#orders+1] = {targetPosition = targetPos, strParam = "", movePosition = pos, endPosition = pos}
-                end
-            end
-        end
-    end
-
-    return orders
-end
-
-function VampiricTouch:getScore(unitId, order)
-    local unit = Wargroove.getUnitById(unitId)
-    local unitClass = Wargroove.getUnitClass(unit.unitClassId)
-    local unitValue = 10
-    
-    local targetUnit = Wargroove.getUnitAt(order.targetPosition)
-    local targetUnitClass = Wargroove.getUnitClass(targetUnit.unitClassId)
-    local targetUnitValue = math.sqrt(targetUnitClass.cost / 100)
-
-    local unitHealth = unit.health
-    local targetHealth = targetUnit.health
-
-    local myHealthDelta = math.min(unitHealth + targetHealth, 100) - unitHealth
-    local myDelta = myHealthDelta / 100 * unitValue
-    local theirDelta = targetHealth / 100 * targetUnitValue
-
-    local score = myDelta + theirDelta
-
-    return { score = score, healthDelta = myHealthDelta, introspection = {
-        { key = "unitValue", value = unitValue },
-        { key = "targetUnitValue", value = targetUnitValue },
-        { key = "myHealthDelta", value = myHealthDelta },
-        { key = "myDelta", value = myDelta },
-        { key = "theirDelta", value = theirDelta }}}
+function VampiricTouch:onPostUpdateUnit(unit, targetPos, strParam, path)
+    GrooveVerb.onPostUpdateUnit(self, unit, targetPos, strParam, path)
+    unit.pos = targetPos
 end
 
 return VampiricTouch
