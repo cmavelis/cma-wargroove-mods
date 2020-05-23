34772038d8281765985d061103d565e9097bf83b
CMAvelis <CMAvelis@gmail.com>
2020-05-22 20:44:22 -0400

    groundwork for the 2nd part of ship movement

---
+++ gow_co_balance_mod/assets_src/lua/initialized/wait.lua
@@ -0,0 +1,29 @@
+local Verb = require "wargroove/verb"
+local OldWait = require "verbs/wait"
+
+local Wait = Verb:new()
+
+-- local doubleTurns = {
+--     "harpoonship": "harpoonship_move_only",
+-- }
+
+-- function Wait:execute(unit, targetPos, strParam, path)
+--     print('We wait')
+    
+-- end
+
+function Wait:init()
+    OldWait.onPostUpdateUnit = Wait.onPostUpdateUnit
+end
+
+function Wait:onPostUpdateUnit(unit, targetPos, strParam, path)
+    print('We wait')
+    if unit.unitClassId == "harpoonship" then
+        unit.unitClassId = "harpoonship_move_only"
+    end
+    -- unit.unitClassId = "commander_twins_water"
+    -- unit.hadTurn = false
+    -- unit.grooveCharge = unit.unitClass.maxGroove
+end
+
+return Wait
