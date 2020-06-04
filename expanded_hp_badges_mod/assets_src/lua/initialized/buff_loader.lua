b66729fcd0f717f0c8f3bab2d4ba3fee7c2f69bb
CMAvelis <CMAvelis@gmail.com>
2020-06-04 16:31:23 -0400

    removing debugging notes

--- expanded_hp_badges_mod/assets_src/lua/initialized/buff_loader.lua
+++ expanded_hp_badges_mod/assets_src/lua/initialized/buff_loader.lua
@@ -7,20 +7,6 @@ local BuffsToLoad = {
 }
 
 -- loading game's original buffs first
-function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
-        end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
 local OldUnitBuffs = require "wargroove/unit_buffs"
 local BaseGameBuffs = require "buffs/vanilla_unit_buffs_2-1"
 
